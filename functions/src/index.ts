790c34d9cf01dca7c6554ec83f831fed14a7e37b
markoboras <mboras1@etfos.hr>
2022-02-03 18:12:11 +0100

    brave and chrome ...

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import functions = require('firebase-functions');
 import admin = require('firebase-admin');
-const serviceAccount = require('../mboras-cloud-messaging-firebase-adminsdk-3crg4-d849258dda.json');
+const serviceAccount = require('../mboras-cloud-messaging-firebase-adminsdk-3crg4-dce7b139ed.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  projectId: 'mboras-cloud-messaging',
 });
 
 const cors = require('cors')({ origin: 'http://localhost:3000' });
@@ -14,13 +13,6 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   cors(request, response, async () => {
     response.header('Access-Control-Allow-Origin', '*');
     console.log('Request body', request.body);
-    // const FCMToken = admin.database().ref(`/FCMTokens/${userId}`).once('value');
-    // const FCMTokenTest = await admin
-    //   .firestore()
-    //   .collection('users')
-    //   .doc(request.body.data.to)
-    //   .get();
-    // console.log('fcm test od usera', FCMTokenTest.data().fcmToken);
 
     const payload = {
       token: request.body.data.otherUserFcmToken,
@@ -45,3 +37,58 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     }
   });
 });
+
+exports.createNewChatTest = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    response.header('Access-Control-Allow-Origin', '*');
+    console.log('Request body', request.body);
+    try {
+      const chatRef = await admin.firestore().collection('messages').add({});
+      const db = admin.firestore();
+      console.log('chat ref cloud functions', chatRef.id);
+      await admin
+        .firestore()
+        .collection('messages')
+        .doc(chatRef.id)
+        .collection('messages')
+        .add({});
+      const userChatRef = admin
+        .firestore()
+        .collection('users')
+        .doc(request.body.data.uid);
+      console.log('userChatRef', userChatRef);
+
+      userChatRef.update({
+        activeChats: firebase.firestore.FieldValue.arrayUnion(chatRef.id),
+      });
+      const receiverChatRef = admin
+        .firestore()
+        .collection('users')
+        .doc(request.body.data.to);
+      receiverChatRef.update({
+        activeChats: firebase.firestore.FieldValue.arrayUnion(chatRef.id),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+/*export const createNewChat = createAsyncThunk(
+  'createNewChat',
+  async (message: Message, { dispatch }) => {
+    try {
+      const chatRef = await addDoc(collection(db, 'messages'), {});
+      await addDoc(collection(db, 'messages', chatRef.id, 'messages'), {});
+      await updateDoc(doc(db, 'users', message.uid), {
+        activeChats: arrayUnion(chatRef.id),
+      });
+      await updateDoc(doc(db, 'users', message.to), {
+        activeChats: arrayUnion(chatRef.id),
+      });
+      dispatch(updateUserChats(message.uid));
+    } catch (error) {
+      throw new Error('didnt send message');
+    }
+  },
+);*/
